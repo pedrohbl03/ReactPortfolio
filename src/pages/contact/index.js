8cd4ceac43532c3e283b37fca6a2b076537e03da
Pedro Lima <pedrozin1234@gmail.com>
2021-11-11 23:39:49 -0300

    in progress edit

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -6,7 +6,7 @@ const ContactPage = () => {
 
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <h2 className="text-6xl font-bold text-primary text-center"> EM CONSTRUÇÃO</h2>
+      <h2 className=" font-bold text-primary text-center"> EM CONSTRUÇÃO</h2>
     </div>
   )
 }
--- src/pages/resume/index.js
+++ src/pages/resume/index.js
@@ -6,7 +6,7 @@ const ResumePage = () => {
 
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <h2 className="text-6xl font-bold text-primary text-center"> EM CONSTRUÇÃO</h2>
+      <h2 className="text-xl font-bold text-primary text-center"> EM CONSTRUÇÃO</h2>
     </div>
   )
 }
